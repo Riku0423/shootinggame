24bbd8f8099f8a2d07a9c27d96f48b485a688c6f
Riku0423 <131140260+Riku0423@users.noreply.github.com>
2024-10-13 10:58:26 +0900

    メッセージ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const fontClass = `${geistSans.variable} ${geistMono.variable}`;
   return (
-    <html lang="en">
-      <body>
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          {children}
-        </div>
-      </body>
+    <html lang="en" className={fontClass}>
+      <body>{children}</body>
     </html>
   );
 }
